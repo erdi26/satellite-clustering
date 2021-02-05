828cc11d665da28cf7ed336bb54c85287f70ee81
erdi <erdi.uenal@adesso.de>
2021-02-05 09:42:15 +0100

    Improved usability

--- main.py
+++ main.py
@@ -33,13 +33,13 @@ def normalize_image(image):
     sum_of_rgb_values = red_values.astype(int) + green_values.astype(int) + blue_values.astype(int)
     sum_of_rgb_values[sum_of_rgb_values == 0] = 1
 
-    result = image.copy()
+    normalized_image = image.copy()
 
-    result[:, :, 0] = red_values / sum_of_rgb_values * 255.0
-    result[:, :, 1] = green_values / sum_of_rgb_values * 255.0
-    result[:, :, 2] = blue_values / sum_of_rgb_values * 255.0
+    normalized_image[:, :, 0] = red_values / sum_of_rgb_values * 255.0
+    normalized_image[:, :, 1] = green_values / sum_of_rgb_values * 255.0
+    normalized_image[:, :, 2] = blue_values / sum_of_rgb_values * 255.0
 
-    return result
+    return normalized_image
 
 
 def cluster(result):
@@ -59,11 +59,11 @@ def generate_clustered_image(labels, image):
     color_red = [255, 0, 0]
     color_green = [0, 255, 0]
     color_blue = [0, 0, 255]
-    color_light_green = [0, 100, 0]
+    color_dark_green = [0, 100, 0]
 
     result[np.where(labels == 2)] = color_blue
     result[np.where(labels == 4)] = color_red
-    result[np.where(labels == 0)] = color_light_green
+    result[np.where(labels == 0)] = color_dark_green
     result[np.where(labels == 1)] = color_green
     result[np.where(labels == 3)] = color_green
 
@@ -76,13 +76,24 @@ def show_image(result):
     plt.show()
 
 
+def save_image(result, name):
+    save_under_path = name + "_clustered.png"
+    plt.imsave(save_under_path, result)
+    print("Result saved as " + save_under_path)
+
+
 if __name__ == '__main__':
-    # access command-line-argument
-    path_to_image = sys.argv[1]
+    if len(sys.argv) > 1:
+        path_to_image = sys.argv[1]
+    else:
+        path_to_image = input("Please enter the image path: ")
 
     image_jpg = read_image_as_jpg(path_to_image)
     image_normalized = normalize_image(image_jpg)
+
     labels = cluster(image_normalized)
     result_image = generate_clustered_image(labels, image_jpg)
+
     show_image(result_image)
+    save_image(result_image, path_to_image)
 
